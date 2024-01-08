7a0f86f6218415d9fc2956cc295abde3fae98c68
hisamuddin01 <hisamuddin357545@gmail.com>
2024-01-08 22:26:39 +0530

    resposive navbar added

---
+++ index.js
@@ -0,0 +1,10 @@
+const navToggle = document.querySelector('.nav__toggle');
+const linksContainer = document.querySelector('.nav__links-container');
+const navLinks = document.querySelector('.nav__links');
+
+// NAVIGATION TOGGLE BUTTON
+navToggle.addEventListener('click', function() {
+    const linksHeight = navLinks.getBoundingClientRect().height;
+    const linksContainerHeight = linksContainer.getBoundingClientRect().height;
+    linksContainerHeight === 0 ? linksContainer.style.height = `${linksHeight}px` : linksContainer.style.height = 0;
+});
