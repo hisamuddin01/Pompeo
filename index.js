9080ed6851e4c0f962db4e3a66123f6064543d0f
hisamuddin01 <hisamuddin357545@gmail.com>
2024-01-11 12:35:57 +0530

    pages added

--- index.js
+++ index.js
@@ -1,39 +1,123 @@
 const collection = [
     {
         id: 1,
-        title:'Decor Plate',
+        title:'Blue Plate',
         price: 65,
         img: './images/collection-01.png'
     },
     {
         id: 2,
-        title:'Mint Pottery',
-        price: 75,
+        title:'White Potterys',
+        price: 115,
         img: './images/collection-02.png'
     },
     {
         id: 3,
-        title:'Set Of Potterys',
-        price: 125,
+        title:'Wine Pottery',
+        price: 95,
         img: './images/collection-03.png'
     },
     {
         id:4,
-        title: 'Orange Ceramic',
+        title: 'White Cup',
         price: 55,
         img: './images/collection-04.png'
     },
     {
         id: 5,
-        title:'Dark Bowl',
+        title:'Red Platte',
         price: 115,
         img: './images/collection-05.png'
     },
     {
         id: 6,
-        title:'Square Pottery',
-        price: 75,
+        title:'Wine Bowl',
+        price: 115,
         img: './images/collection-06.png'
+    },
+    {
+        id: 7,
+        title:'Gray Cup',
+        price: 65,
+        img: './images/collection-07.png'
+    },
+    {
+        id: 8,
+        title:'Decor Plate',
+        price: 65,
+        img: './images/collection-08.png'
+    },
+    {
+        id: 9,
+        title:'Mint Pottery',
+        price: 75,
+        img: './images/collection-09.png'
+    },
+    {
+        id: 10,
+        title:'Set of Potterys',
+        price: 125,
+        img: './images/collection-10.png'
+    },
+    {
+        id: 11,
+        title:'Orange Ceramic',
+        price: 55,
+        img: './images/collection-11.png'
+    },
+    {
+        id: 12,
+        title:'Dark Bowl',
+        price: 115,
+        img: './images/collection-12.png'
+    },
+    {
+        id: 13,
+        title:'Squre Pottery',
+        price: 75,
+        img: './images/collection-13.png'
+    },
+    {
+        id: 14,
+        title:'Black Pottery',
+        price: 115,
+        img: './images/collection-14.png'
+    },
+    {
+        id: 15,
+        title:'Torguers Vaze',
+        price: 95,
+        img: './images/collection-15.png'
+    },
+    {
+        id: 16,
+        title:'Silver Pottery',
+        price: 100,
+        img: './images/collection-16.png'
+    },
+    {
+        id: 17,
+        title:'Mint Bowl',
+        price: 115,
+        img: './images/collection-07.png'
+    },
+    {
+        id: 18,
+        title:'Gold & Black Pottery',
+        price: 95,
+        img: './images/collection-18.png'
+    },
+    {
+        id: 19,
+        title:'Orange Plate',
+        price: 65,
+        img: './images/collection-19.png'
+    },
+    {
+        id: 20,
+        title:'Red Ceramic',
+        price: 45,
+        img: './images/collection-20.png'
     }
 ]
 
